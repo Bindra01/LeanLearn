870489c072696c10ec821f90c3525ec266d17509
Nmait2111 <namitjain2111@gmail.com>
2025-02-08 22:11:58 +0530

    sx

--- client/src/lib/api/utils.ts
+++ client/src/lib/api/utils.ts
@@ -1,6 +1,6 @@
 import { CommonQty } from "@/types/utilsInteface";
 
-const BASE_URL = 'https://lean-learn-backend-ai-do7a.onrender.com';
+const BASE_URL = 'http://127.0.0.1:8000';
 export const quantitiesApi = {
   getAll: async (): Promise<CommonQty[]> => {
     try {
